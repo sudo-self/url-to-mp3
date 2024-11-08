62d8113c7972b3633dfc68d599debdc300e8787a
Sudo-Self <sudo-self@proton.me>
2024-11-08 08:55:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def convert_url_to_mp3():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(port=5000)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
 
